f6d5bd767e8e897f4fbc1e2d84eda18194c3ff3c
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-03-05 14:29:15 +0200

    fix regular exp

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { FormStyled, FormLabel, Input, Button } from './ContactForm.styled';
 
-const nameRegExp = `^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$`;
-const phoneRegExp = `\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}`;
+const nameRegExp = "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
+const phoneRegExp =
+  '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}';
 
 export const ContactForm = ({ onAddContact }) => {
   const [name, setName] = useState('');
